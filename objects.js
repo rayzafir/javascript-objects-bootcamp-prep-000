9028b9ee06b74d72f2448928d92bee4496465e76
Ray Raven <rayzafir@aol.com>
2018-08-22 23:42:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,9 @@ var playlist = {artistName: 'songTitle'};
 function updatePlaylist (playlist, artistName, songTitle) {
 playlist [artistName] = 'songTitle'
 }
+function removeFromPlaylist(playlist,artistname){
+delete playlist.artistName
+}
 
 
 
